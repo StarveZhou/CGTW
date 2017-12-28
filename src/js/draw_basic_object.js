dcd6a8c3b977f0192aef87ab9e19f176cb7cd939
ZhaoXiangyu <3230286230@qq.com>
2017-12-28 15:24:04 +0800

    No thing special

--- src/js/draw_basic_object.js
+++ src/js/draw_basic_object.js
@@ -1,5 +1,5 @@
-function drawPolygon(gl, programInfo, projectionMatrix, positions, faceColors, indices, translation, scale, rotation) {
-    const buffers = initBuffers(gl, positions, faceColors, indices);
+function drawPolygon(gl, programInfo, projectionMatrix, positions, faceColors, indices, translation, scale, rotation, texture, textureCoordinates,vertexNormals) {
+    const buffers = initBuffers(gl, positions, faceColors, indices, textureCoordinates, vertexNormals);
 
     const modelViewMatrix = mat4.create();
 
@@ -41,6 +41,11 @@ function drawPolygon(gl, programInfo, projectionMatrix, positions, faceColors, i
             [0, 0, 1]);       // axis to rotate around (Z)
     }
 
+    const normalMatrix = mat4.create();
+    // normal matrix should transform the invert transpose matrix of modelViewMatrix
+    mat4.invert(normalMatrix,modelViewMatrix);
+    mat4.transpose(normalMatrix,normalMatrix);
+
     // Tell WebGL how to pull out the positions from the position
     // buffer into the vertexPosition attribute
     {
@@ -79,6 +84,45 @@ function drawPolygon(gl, programInfo, projectionMatrix, positions, faceColors, i
         gl.enableVertexAttribArray(programInfo.attribLocations.vertexColor);
     }
 
+    // Tell WebGL how to pull out the texture coordinates from
+    // the texture coordinate buffer into the textureCoord attribute.
+    if (textureCoordinates)
+    {
+      const numComponents = 2;
+      const type = gl.FLOAT;
+      const normalize = false;
+      const stride = 0;
+      const offset = 0;
+      gl.bindBuffer(gl.ARRAY_BUFFER, buffers.textureCoord);
+      gl.vertexAttribPointer(
+          programInfo.attribLocations.textureCoord,
+          numComponents,
+          type,
+          normalize,
+          stride,
+          offset);
+      gl.enableVertexAttribArray(
+          programInfo.attribLocations.textureCoord);
+    }
+
+     // Tell WebGL how to pull out the normals 
+    {
+      const numComponents = 3;
+      const type = gl.FLOAT;
+      const normalize = false;
+      const stride = 0;
+      const offset = 0;
+      gl.bindBuffer(gl.ARRAY_BUFFER, buffers.normal);
+      gl.vertexAttribPointer(
+          programInfo.attribLocations.vertexNormal,
+          numComponents,
+          type,
+          normalize,
+          stride,
+          offset);
+      gl.enableVertexAttribArray(
+          programInfo.attribLocations.vertexNormal);
+    }
 
     // Tell WebGL which indices to use to index the vertices
     gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, buffers.indices);
@@ -95,6 +139,20 @@ function drawPolygon(gl, programInfo, projectionMatrix, positions, faceColors, i
         programInfo.uniformLocations.modelViewMatrix,
         false,
         modelViewMatrix);
+    gl.uniformMatrix4fv(
+        programInfo.uniformLocations.normalMatrix,
+        false,
+        normalMatrix);
+
+    if (texture)
+    {
+        // Tell WebGL we want to affect texture unit 0
+        gl.activeTexture(gl.TEXTURE0);
+        // Bind the texture to texture unit 0
+        gl.bindTexture(gl.TEXTURE_2D, texture);
+        // Tell the shader we bound the texture to texture unit 0
+        gl.uniform1i(programInfo.uniformLocations.uSampler, 0);
+    }
     {
         const vertexCount = indices.length;
         const type = gl.UNSIGNED_SHORT;
@@ -458,3 +516,85 @@ function initBuffers(gl, positions, faceColors, indices, textureCoordinates) {
         color: colorBuffer,
         indices: indexBuffer,
     };
+}
+
+//
+// Initialize a shader program, so WebGL knows how to draw our data
+//
+function initShaderProgram(gl, vsSource, fsSource) {
+    const vertexShader = loadShader(gl, gl.VERTEX_SHADER, vsSource);
+    const fragmentShader = loadShader(gl, gl.FRAGMENT_SHADER, fsSource);
+    const shaderProgram = gl.createProgram();
+    gl.attachShader(shaderProgram, vertexShader);
+    gl.attachShader(shaderProgram, fragmentShader);
+    gl.linkProgram(shaderProgram);
+    if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
+        alert('Unable to initialize the shader program: ' + gl.getProgramInfoLog(shaderProgram));
+        return null;
+    }
+
+    return shaderProgram;
+}
+
+//
+// creates a shader of the given type, uploads the source and
+// compiles it.
+//
+function loadShader(gl, type, source) {
+    const shader = gl.createShader(type);
+    gl.shaderSource(shader, source);
+    gl.compileShader(shader);
+    if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+        alert('An error occurred compiling the shaders: ' + gl.getShaderInfoLog(shader));
+        gl.deleteShader(shader);
+        return null;
+    }
+
+    return shader;
+}
+
+// the function is needed when loading texture
+// use bitwise and 
+function isPowerOf2(value) {
+  return (value & (value - 1)) == 0;
+}
+
+// load the texture from given url 
+// note that you should enable read local file in your browser 
+// return texture
+function loadTexture(gl, url) {
+  const texture = gl.createTexture();
+  gl.bindTexture(gl.TEXTURE_2D, texture);
+
+  const level = 0;
+  const internalFormat = gl.RGBA;
+  const width = 1;
+  const height = 1;
+  const border = 0;
+  const srcFormat = gl.RGBA;
+  const srcType = gl.UNSIGNED_BYTE;
+  const pixel = new Uint8Array([0, 0, 255, 255]);  // opaque blue
+  gl.texImage2D(gl.TEXTURE_2D, level, internalFormat,
+                width, height, border, srcFormat, srcType,
+                pixel);
+
+  var image = new Image();
+  image.onload = function() {
+    gl.bindTexture(gl.TEXTURE_2D, texture);
+    gl.texImage2D(gl.TEXTURE_2D, level, internalFormat,
+                  srcFormat, srcType, image);
+
+    // if size of image is not power of 2, should do something 
+    if (isPowerOf2(image.width) && isPowerOf2(image.height)) {
+       gl.generateMipmap(gl.TEXTURE_2D);
+    } else {
+       // wrapping to clamp to edge
+       gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+       gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+       gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+    }
+  };
+  image.src = url;
+
+  return texture;
+}

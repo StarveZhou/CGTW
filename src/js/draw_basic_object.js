52f07dc58f1115954e9b93f4c3390c98d3a5e19d
ZhaoXiangyu <3230286230@qq.com>
2018-01-04 19:52:36 +0800

    add color

--- src/js/draw_basic_object.js
+++ src/js/draw_basic_object.js
@@ -432,7 +432,7 @@ function drawCube(gl, programInfo, projectionMatrix, object, ambientLight, light
     ];
     object.positions = vertexPositionData;
     object.indices = indexData;
-    object.vertexNormal = normalData;
+    object.vertexNormals = normalData;
     object.normalIndices = normalIndex;
     object.textureIndices = textureIndex;
     //drawPolygon(gl, programInfo, projectionMatrix, vertexPositionData, [[1, 1, 1, 1]], indexData, translation, scale, rotation);
@@ -505,7 +505,7 @@ function drawSphere(gl, programInfo, projectionMatrix, object, ambientLight, lig
     }
     object.positions = vertexPositionData;
     object.indices = indexData;
-    object.vertexNormal = normalData;
+    object.vertexNormals = normalData;
     object.normalIndices = normalIndex;
     object.textureCoordinates = textureCoordData;
     object.textureIndices = textureIndex;
@@ -836,6 +836,8 @@ function initBuffers(gl, object) {
         real_vertexNormals.push(object.vertexNormals[pos + 1]);
         real_vertexNormals.push(object.vertexNormals[pos + 2]);
     }
+    console.log(object.vertexNormals);
+    // console.log(real_vertexNormals);
     // the normal buffer used for lighting 
     const normalBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, normalBuffer);

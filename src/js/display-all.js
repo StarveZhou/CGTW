1b9b457402ab26906b1774094ef6ef0c0bbf3768
StarveZhou <18645488596@163.com>
2018-01-12 14:54:21 +0800

    修复obj没有定义的情况

--- src/js/display-all.js
+++ src/js/display-all.js
@@ -205,7 +205,7 @@ function drawScene(gl, programInfo, matrixInfo, ambientLight, lightSources) {
                 drawTrustumOfAPyramid(gl, programInfo, matrixInfo, object, ambientLight, lightSources, BufferPool[item]);
                 break;
             case "model":
-                if (mType !== "model" || ObjectPool[name].objFile !== null) {
+                if (ObjectPool[name].objFile !== null) {
                     objDisplay(gl, programInfo, matrixInfo, object, ambientLight, lightSources, BufferPool[item]);
                 }
                 break;
